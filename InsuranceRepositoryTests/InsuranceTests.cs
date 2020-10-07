e55826d4b2f658cbf62063f2ddfc25111491c12f
unknown <asantaems@gmail.com>
2020-10-07 18:12:02 -0400

    tried to add tests. some worked. some didnt. help.

---
+++ InsuranceRepositoryTests/InsuranceTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using KomodoInsuranceLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InsuranceRepositoryTests
+{
+    [TestClass]
+    public class InuranceTests
+    {
+        [TestMethod]
+        public void SetProperty_ShouldSet()
+        {
+            InsuranceItems test = new InsuranceItems();
+            test.SetDoorNames("D4", "D5");
+            test.SetEmployeeNumber(123456);
+
+        }
+    }
+}
